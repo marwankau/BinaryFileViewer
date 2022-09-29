926cf2a03049d58481cfccd07267afd0692a039b
Yahya <44480217+y7ya@users.noreply.github.com>
2022-09-29 08:42:07 +0300

    new: add Read as Binary File

--- src/FileViewerApp.java
+++ src/FileViewerApp.java
@@ -1,11 +1,7 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.util.Scanner;
+
 
 import javax.swing.JFileChooser;
 
